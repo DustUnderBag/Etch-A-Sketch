d20ef7226013b757f5d106bb21278d1f1eda0116
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2023-05-07 13:28:16 +0800

    Organise tile generation and filling code to functions

--- script.js
+++ script.js
@@ -2,29 +2,35 @@ const canvas = document.querySelector('.canvas');
 let tiles = [];
 
 const canvasSize = 500;
-let tileNum = 16;
+
+let tileNum = Number(prompt("Please enter Number only!", 16));
+
 let tileSize = canvasSize / tileNum + "px";
 console.log(tileSize);
 
 canvas.style.width = canvasSize + "px";
 canvas.style.height = canvasSize + "px";
 
-for(let i = 0; i < tileNum * tileNum; i++) {
-    let tileIndex = "tile" + i;
-    tiles[i] = document.createElement('div');
-    tiles[i].classList.add('tile');
-    tiles[i].setAttribute("id", tileIndex);
-
-    tiles[i].style.width = tileSize;
-    tiles[i].style.height = tileSize;
-
-    canvas.appendChild(tiles[i]);
-
-    console.log(tileIndex);
-}
+createTiles();
 
 tiles.forEach(tile => {
-    tile.addEventListener('mouseover', e => {
-        e.target.classList.add("filled");
-    });
+    tile.addEventListener( "mouseover", e => fillTiles(e) );
 });
+
+function createTiles() {
+    for(let i = 0; i < tileNum * tileNum; i++) {
+        let tileIndex = "tile" + i;
+        tiles[i] = document.createElement('div');
+        tiles[i].classList.add('tile');
+        tiles[i].setAttribute("id", tileIndex);
+    
+        tiles[i].style.width = tileSize;
+        tiles[i].style.height = tileSize;
+    
+        canvas.appendChild(tiles[i]);
+    }
+}
+
+function fillTiles(e) {
+    e.target.classList.add("filled");
+}
