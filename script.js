21b594e22f17a383161f3b606ba4b4e971a29db3
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2023-05-07 12:42:25 +0800

    Create .canvas and set its dimension in js

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('.canvas');
+
+const canvasSize = 500;
+let tileNum = 16;
+let tileSize = canvasSize / tileNum;
+
+canvas.style.width = canvasSize + "px";
+canvas.style.height = canvasSize + "px";
+
