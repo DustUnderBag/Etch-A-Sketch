9e32a50531cb84c6575cf9f7f5ce1ca59cb43439
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2023-05-07 13:02:54 +0800

    Created grid of divs in .canvas w/ loop and flexbox

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const canvas = document.querySelector('.canvas');
+let tiles = [];
 
 const canvasSize = 500;
 let tileNum = 16;
-let tileSize = canvasSize / tileNum;
+let tileSize = canvasSize / tileNum + "px";
+console.log(tileSize);
 
 canvas.style.width = canvasSize + "px";
 canvas.style.height = canvasSize + "px";
 
+for(let i = 0; i < tileNum * tileNum; i++) {
+    let tileIndex = "tile" + i;
+    tiles[i] = document.createElement('div');
+    tiles[i].classList.add('tile');
+    tiles[i].setAttribute("id", tileIndex);
+
+    tiles[i].style.width = tileSize;
+    tiles[i].style.height = tileSize;
+
+    canvas.appendChild(tiles[i]);
+
+    console.log(tileIndex);
+}
